56176858ffa361ec9354dc3187f9b46a713c5102
Amit Rathore <amitrathore@gmail.com>
2018-10-16 11:45:40 +0000

    added scala mode etc

--- init.el
+++ init.el
@@ -153,7 +153,7 @@
  '(coffee-tab-width 2)
  '(package-selected-packages
    (quote
-    (twittering-mode haskell-mode tagedit smex rainbow-delimiters projectile paredit magit ido-ubiquitous exec-path-from-shell clojure-mode-extra-font-locking cider))))
+    (ensime twittering-mode haskell-mode tagedit smex rainbow-delimiters projectile paredit magit ido-ubiquitous exec-path-from-shell clojure-mode-extra-font-locking cider))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
